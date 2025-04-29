b5a7915934601787d70573a0b592e3b5d926dab0
Evin <guoshamin@gmail.com>
2025-04-29 15:25:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use('/', createProxyMiddleware({
   pathRewrite: { '^/': '/' },
   onProxyReq: (proxyReq, req, res) => {
     // 这里自动加上你的 OpenAI API Key
-    proxyReq.setHeader('Authorization', 'Bearer 你的sk-xxx'); 
+    proxyReq.setHeader('Authorization', 'Bearer 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'); 
   }
 }));
 
